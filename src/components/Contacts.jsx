c885bd91cfb0077c9825d0d72bf6b9dc62428967
Sumin Shrestha <156720076+samvaarv@users.noreply.github.com>
2024-10-23 17:04:58 -0400

    Update Contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Contacts() {
   return (
     <div id="contact" className="w-100">
       <span>Got a project? Need an unfair advantage?</span>
-      <a aria-label="Get in touch" className="relative" href="/contact">
+      <Link to="/contact" aria-label="Get in touch" className="relative">
         <img className="w-100" src="images/contact.png" alt="Get in Touch" />
-      </a>
+      </Link>
     </div>
   );
 }
